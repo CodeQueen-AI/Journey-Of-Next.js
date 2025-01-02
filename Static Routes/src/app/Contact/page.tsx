38ede08ba987f28e11db3aae1e45eee7b2b537d5
Sumbal Naz <Codeq209@gmail.com>
2025-01-02 23:14:45 +0500

    Add files via upload

---
+++ Static Routes/src/app/Contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gradient-to-tl from-green-800 via-green-300 to-green-600 text-6xl font-bold flex items-center justify-center h-screen'>
+      Contact Page
+    </div>
+  )
+}
+
+export default page
---
+++ Static Routes/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gradient-to-bl from-pink-950 via-pink-300 to-pink-600 text-6xl font-bold flex items-center justify-center h-screen'>
+      Home Page
+    </div>
+  )
+}
+
+export default page
